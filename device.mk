2692d0e0bef7fca377cab59e7e2c519a7001151f
Devin Moore <devinmoore@google.com>
2021-11-08 17:52:47 +0000

    nx563j: Switch to new AIDL IR service
    Test: atest VtsHalIrTargetTest hal_implementation_test
    Bug: 205000342
    Change-Id: I70c54aa8e8962939cc8f4b42fbbe828e9dad7f0d
    Signed-off-by: basamaryan <basam.aryan@gmail.com>

--- device.mk
+++ device.mk
@@ -43,8 +43,7 @@ PRODUCT_PACKAGES += \
 
 # Consumerir
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir-service.example
 
 # Consumerir
 PRODUCT_COPY_FILES += \
